3bcd07e4def6fabbb9649ae142f45ac09be6374d
qiyangzh <57245620+qiyangzh@users.noreply.github.com>
2021-10-09 21:10:48 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,86 +1,79 @@
 library(shiny)
-
-# Define UI for slider demo app ----
 ui <- fluidPage(
-
-  # App title ----
-  titlePanel("Sliders"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar to demonstrate various slider options ----
-    sidebarPanel(
-
-      # Input: Simple integer interval ----
-      sliderInput("integer", "Integer:",
-                  min = 0, max = 1000,
-                  value = 500),
-
-      # Input: Decimal interval with step value ----
-      sliderInput("decimal", "Decimal:",
-                  min = 0, max = 1,
-                  value = 0.5, step = 0.1),
-
-      # Input: Specification of range within an interval ----
-      sliderInput("range", "Range:",
-                  min = 1, max = 1000,
-                  value = c(200,500)),
-
-      # Input: Custom currency format for with basic animation ----
-      sliderInput("format", "Custom Format:",
-                  min = 0, max = 10000,
-                  value = 0, step = 2500,
-                  pre = "$", sep = ",",
-                  animate = TRUE),
-
-      # Input: Animation with custom interval (in ms) ----
-      # to control speed, plus looping
-      sliderInput("animation", "Looping Animation:",
-                  min = 1, max = 2000,
-                  value = 1, step = 10,
-                  animate =
-                    animationOptions(interval = 300, loop = TRUE))
-
+    # *Input() functions
+    # selectInput(inputId = "Evidence", 
+    #             label = "Choose an evidence category", 
+    #             choices = c("Evidence Present", "Evidence Absent")),
+    selectInput(inputId = "Rating", 
+                label = "Choose level of rating",
+                choices = c("All", "Strong", "Promising", "Moderate", "No significance")),
+    selectInput(inputId = "Topic",
+                label = "Choose a topic",
+                choices = c("All", "Math", "Reading")),
+    selectInput(inputId = "Grade",
+                label = "Choose a grade",
+                choices = c("All", "Kindergarten", "Elementary", "Middle", "High")),
+    fluidRow(
+        column(width = 12,
+               plotOutput("contents", hover = hoverOpts(id ="plot_hover"))
+        )
     ),
-
-    # Main panel for displaying outputs ----
+    fluidRow(
+        column(width = 12,
+               verbatimTextOutput("hover_info")
+        )
+    ),
+    # *Output() functions
     mainPanel(
-
-      # Output: Table summarizing the values entered ----
-      tableOutput("values")
-
+        textOutput(outputId = "size"),
+        uiOutput('map')
     )
-  )
 )
 
-# Define server logic for slider examples ----
 server <- function(input, output) {
+    source("e4e_egm.R")
+    orig <- load_E4E()
+    # output$size <- renderText({
+    #     paste("size: ", height(), sep = "")
+    # })
+    
+    df <- reactive({
+        egm_df <- prep_egm(apply_choices(orig, input$Rating, input$Topic, input$Grade))
+        make_egm(egm_df)   
+    })
+    
+    height <- reactive({
+        egm_df <- prep_egm(apply_choices(orig, input$Rating, input$Topic, input$Grade))
+        # if(input$detail=="Programs"){
+            length(unique(egm_df$name))*150
+        # } else {
+        #     length(unique(egm_df$Program))*600   # If not programs, then it's types of programs
+        # }
+    })
 
-  # Reactive expression to create data frame of all input values ----
-  sliderValues <- reactive({
-
-    data.frame(
-      Name = c("Integer",
-               "Decimal",
-               "Range",
-               "Custom Format",
-               "Animation"),
-      Value = as.character(c(input$integer,
-                             input$decimal,
-                             paste(input$range, collapse = " "),
-                             input$format,
-                             input$animation)),
-      stringsAsFactors = FALSE)
-
-  })
-
-  # Show the values in an HTML table ----
-  output$values <- renderTable({
-    sliderValues()
-  })
-
+    output$contents <- renderPlot({
+        df()
+    })
+    
+    output$map <- renderUI({
+        # fillPage(plotOutput("contents", height = height(), width = "100%"))
+        #plotOutput("contents", height = "200", width = "100%")
+        plotOutput("contents", height = height(), width = "100%")
+    })
+    
+    output$hover_info <- renderPrint({
+        if(!is.null(input$plot_hover)){
+            plotdf <- prep_egm(apply_choices(orig, input$Rating, input$Topic, input$Grade))
+            hover=input$plot_hover
+            dist=sqrt((hover$x-plotdf$size)^2+(hover$y - plotdf$`Effect Size`)^2)
+            #cat(" x =", hover$x, "y =", hover$y, "\n",
+            #    "min dist =", min(dist), "\n",
+            #    "which = ", which.min(dist), "\n",
+            cat(paste(plotdf$name[which.min(dist)]), paste(plotdf$url[which.min(dist)]), sep="\n")
+        }
+        
+        
+    })
 }
 
-# Create Shiny app ----
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
